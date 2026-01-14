4db59fafa3b43b4496e41923783bbd0db3f6efcc
gjthec <jgtecnologiasauto@gmail.com>
2026-01-14 00:25:37 -0300

    Allow alunoId instead of matricula

--- backend/src/infra/http/validators/registro.validator.ts
+++ backend/src/infra/http/validators/registro.validator.ts
@@ -2,7 +2,14 @@ import { check, query } from "express-validator";
 
 export const createNewRegistroValidator = [
   check("cursoId").notEmpty().withMessage("cursoId is required"),
-  check("matricula").notEmpty().withMessage("matricula is required"),
+  check("matricula")
+    .if((value, { req }) => !req.body.alunoId)
+    .notEmpty()
+    .withMessage("matricula is required"),
+  check("alunoId")
+    .if((value, { req }) => !req.body.matricula)
+    .notEmpty()
+    .withMessage("alunoId is required"),
 
   // foto (pictureLocation) – se você salva como objeto/json
   check("foto").notEmpty().withMessage("foto is required"),
